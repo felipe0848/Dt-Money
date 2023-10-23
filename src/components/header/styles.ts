cf656f017b62e678c96d62903bf5a5a3f35b4ec6
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-23 15:38:13 -0400

    Feat: Create Header Component

---
+++ src/components/header/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  background-color: ${(props) => props.theme['gray-900']};
+  padding: 2.5rem 0 7.5rem;
+`
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NewTransactionButton = styled.button`
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+
+  padding: 0.75rem 1.25rem;
+  border: none;
+  border-radius: 6px;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme['green-700']};
+    transition: background-color 0.2s;
+  }
+`
