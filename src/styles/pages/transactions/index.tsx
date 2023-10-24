40aa86c851fc409ecf9a1e55fe813f39ccc7620e
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-24 12:35:21 -0400

    Feat: Add table transactions

--- src/styles/pages/transactions/index.tsx
+++ src/styles/pages/transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../../components/header'
 import { Summary } from '../../../components/summary'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
